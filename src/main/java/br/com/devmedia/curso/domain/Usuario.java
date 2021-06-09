b5d461a2a92fd16558ffdbd9b1cdb02a013c6599
LauraEllen17 <laura.ellen@inovafarm.com.br>
2021-06-09 09:50:37 -0300

    bean de validação

--- src/main/java/br/com/devmedia/curso/domain/Usuario.java
+++ src/main/java/br/com/devmedia/curso/domain/Usuario.java
@@ -2,20 +2,42 @@ package br.com.devmedia.curso.domain;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public class Usuario {
+	/*
+	 * Depois q adicionamos a dependencia de hibernate-validator no pom
+	 * podemos colcoar asa anotações de validação.
+	 */
 	
 	private Long id;
+	
+	/*
+	 * a anotação notBlank adiciona uma validação de que não é possivel o campo nome estar em branco;
+	 * a anotação size limita o tamanho do campo
+	 * 
+	 */
+	@NotBlank
+	@Size(min = 3, max = 50)
 	private String nome;
+	
+	@NotBlank
+	@Size(min = 3, max = 50, message = "Campo obrigatório entre {min} e {max} caracteres.")
 	private String sobrenome;
+	
 	private TipoSexo sexo;
+	
 	/*
 	 * Essa anotação transforma o retorno que vem como string, em um objeto.
 	 * Pois o spring espera um objeto
 	 */
 	@DateTimeFormat(iso = ISO.DATE)
+	@NotNull(message = "O campo data de nascimento é obrigatório.")
 	private LocalDate dtNascimento;
 	
 	//método construtor padrão, pois quando trabalhamos com spring MVC, 
--- src/main/java/br/com/devmedia/curso/web/controller/UsuarioController.java
+++ src/main/java/br/com/devmedia/curso/web/controller/UsuarioController.java
@@ -1,8 +1,11 @@
 package br.com.devmedia.curso.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -78,9 +81,17 @@ public class UsuarioController {
 	 * no formulario, é do tipo Usuario.
 	 * 
 	 * Quando o formulario enviar os dados, recebemos ja como um objeto;
+	 * 
+	 * a anotação @Valid diz ao spring pra ele usar a validação do Bean Validation;
+	 * 
+	 * BindignResult é para usar para alterar o fluxo para caso tenha algum tipo de erro (caminho)
+	 * e tem que estar obrigatoriamente antes do redirect
 	 */
 	@PostMapping("/save")
-	public String save(@ModelAttribute("usuario") Usuario usuario, RedirectAttributes attr) {
+	public String save(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, RedirectAttributes attr) {
+		if(result.hasErrors()) {
+			return "/user/add";
+		}
 		dao.salvar(usuario);
 		attr.addFlashAttribute("message", "Usuário salvo com sucesso");
 		return "redirect:/usuario/todos";
@@ -107,7 +118,10 @@ public class UsuarioController {
 	 */
 	
 	@PostMapping("/update")
-	public ModelAndView update(@ModelAttribute("usuario") Usuario usuario, RedirectAttributes attr) {
+	public ModelAndView update(@ModelAttribute("usuario") Usuario usuario, BindingResult result, RedirectAttributes attr) {
+		if(result.hasErrors()) {
+			return new ModelAndView("/user/add");
+		}
 		dao.editar(usuario);
 		attr.addFlashAttribute("message", "Usuário alterado com sucesso");
 		return new ModelAndView("redirect:/usuario/todos");
